2ff53d4a667239b548eedc700cdd99887cc54bff
Nishant Verma <nishant.1.verma@morrisonsplc.co.uk>
2019-01-05 11:58:29 +0000

    New Commit

---
+++ training/Welcome.py
@@ -0,0 +1,12 @@
+print("Welcome")
+a = 20
+print(a)
+a = "String"
+print(a)
+
+tu=(1,3,6,8)
+print(tu[1:3])
+
+l=[1,2,4,5]
+print(3 in l)
+
---
+++ training/basics.py
@@ -0,0 +1,8 @@
+'''
+
+Created On Jan 5, 2019
+
+'''
+if __name__ == '__main__':
+    print("Welcome to Python Programming")
+    pass
